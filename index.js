30a6a9c2a2192d7bc8745cec35cd6b416b78afa1
luolisave <luolisave@gmail.com>
2022-02-20 14:24:13 -0500

    add chat (socket.io)

--- index.js
+++ index.js
@@ -3,12 +3,37 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  // socket.on('chat message', (msg) => {
+  //   console.log('message: ' + msg);
+  // });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+
+/* 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+});
+//*/
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
